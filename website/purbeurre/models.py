1aaa306438edcbf4100187cd56428dc24aef795c
LAPTOP-FAVNB76U\shequ <shequet@gmail.com>
2021-01-18 23:32:15 +0100

    no message

--- website/purbeurre/models.py
+++ website/purbeurre/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    code = models.CharField(null=False, max_length=13)
+    owner = models.ForeignKey(User, related_name='products', on_delete=models.CASCADE)
+    name = models.CharField(max_length=128, blank=False, null=True)
+    nutri_score = models.IntegerField(null=False)
+
+    def __str__(self):
+        return '%s' % (self.name, )
--- website/purbeurre/urls.py
+++ website/purbeurre/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('myfood/', views.myfood, name='myfood'),
+    path('product/search/', views.product_search, name='product_search'),
+    path('product/<int:product_id>/', views.product_detail, name='product_detail'),
 ]
 
