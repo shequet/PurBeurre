cd2bac88356fbeff838cddfcb040f9976db7baec
LAPTOP-FAVNB76U\shequ <shequet@gmail.com>
2021-01-18 23:37:33 +0100

    no message

--- website/purbeurre/models.py
+++ website/purbeurre/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Product(models.Model):
     code = models.CharField(null=False, max_length=13)
     owner = models.ForeignKey(User, related_name='products', on_delete=models.CASCADE)
-    name = models.CharField(max_length=128, blank=False, null=True)
+    name = models.CharField(max_length=128, blank=False, null=False)
     nutri_score = models.IntegerField(null=False)
 
     def __str__(self):
