99e4dbdea14f768d9fb085d247a280f449758845
shequet <shequet@gmail.com>
2021-02-09 00:15:28 +0100

    debug false

--- website/views.py
+++ website/views.py
@@ -15,7 +15,7 @@ def register(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect(reverse('home'))
+            return redirect(reverse('profile'))
 
 
 def profile(request):
