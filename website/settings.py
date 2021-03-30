b75617866f885d226a698136b431e373476c6324
shequet <shequet@gmail.com>
2021-03-30 21:42:51 +0200

    remove comment sentry logs

--- website/settings.py
+++ website/settings.py
@@ -18,14 +18,7 @@ from sentry_sdk.integrations.django import DjangoIntegration
 sentry_sdk.init(
     dsn="https://03fa9aa8b73d42eab9a63a28f50713db@o87878.ingest.sentry.io/191402",
     integrations=[DjangoIntegration()],
-
-    # Set traces_sample_rate to 1.0 to capture 100%
-    # of transactions for performance monitoring.
-    # We recommend adjusting this value in production.
     traces_sample_rate=1.0,
-
-    # If you wish to associate users to errors (assuming you are using
-    # django.contrib.auth) you may enable sending PII data.
     send_default_pii=True
 )
 
